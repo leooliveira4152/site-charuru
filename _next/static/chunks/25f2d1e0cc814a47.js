efc4ec3b1bda2b4491002907508b47f29df37fce
Leonardo Oliveira <leo@ryft.vc>
2025-10-11 15:49:28 -0300

    Updates

--- _next/static/chunks/25f2d1e0cc814a47.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/6b297c6bc770653d.js",
-  "static/chunks/bc2c603851ee52cc.js",
-  "static/chunks/turbopack-d6d5cf4099e571b8.js"
-])
--- _next/static/chunks/bba01661dfa43195.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/5a6c3db863a354ce.js",
-  "static/chunks/bc2c603851ee52cc.js",
-  "static/chunks/turbopack-6eef5b1629fbd9c5.js"
-])
